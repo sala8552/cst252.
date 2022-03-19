36aeac8479864fb87dbf24668abc6cc4701e7ceb
sala8552 <70294977+sala8552@users.noreply.github.com>
2022-03-19 16:17:43 -0700

    lab 14

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,17 +1,13 @@
-/*
- * Author: Wes Modes <wmodes@csumb.edu>
- * Created: 19 September
- * License: Public Domain
- */
+/**
+    * Author:  Elias Salas
+    * Created: 03.19.2022
+    * License: Public Domain
+**/
 
 maxFactors = 4;
 
 outputEl = document.getElementById("output");
 
-// get the values from the webpage and write them in an object
-// this expects to have input fields with ids num0, text0, num1, text1, etc
-// returns an object that looks like this:
-//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
 function getFactorObj() {
     var factorObj = {};
     for (var factor=0; factor<maxFactors; factor++) {
@@ -40,19 +36,19 @@ function outputToPage(str) {
 // loops over the numbers and outputs the number and the matching text
 // for factors
 function fizzBuzzBoom(maxNums, factorObj) {
-    // iterate over all of out numbers
+
     for (var num=0; num<maxNums; num++) {
-        // reset output string
+
         var outputStr = "";
-        // iterate over the factors we got from the html
+
         for (var factor in factorObj) {
-            // check to see if this num is a multiple of factor
+
             if (num % factor == 0) {
-                // if yes, than add the text to output string
+
                 outputStr += factorObj[factor];
             }
         }
-        // now if we have words in outputStr, format it like this " - FizzBuzz!"
+
         if (outputStr) {
             outputStr = " - " + outputStr + "!";
         }
